9a13df8fcc484a44de89ae3c52a2e3af0c93c51c
amber <amber@thedemoapp.com>
2023-04-18 11:57:48 +0530

    basic standalone

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.projects.app',
+  appName: 'converter',
+  webDir: 'www',
+  bundledWebRuntime: false
+};
+
+export default config;
